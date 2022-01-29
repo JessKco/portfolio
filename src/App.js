eca6646dea932ceda346b36987ba289937505f15
JessKco <89395463+JessKco@users.noreply.github.com>
2022-01-29 22:23:33 +0000

    Added all primary nav links to home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Welcome to my world
-       </h1>
-      </header>
+      <Router>
+        <div className="links">
+          <nav className="color-nav">
+            <Link to="/" className="link">
+              Home |
+            </Link>{" "}
+            <Link to="about" className="link">
+              About |
+            </Link>{" "}
+            <Link to="projects" className="link">
+              Projects |
+            </Link>{" "}
+            <Link to="contact" className="link">
+              Contact
+            </Link>
+          </nav>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css"
+
+export default function Home(){
+return (
+  <div className="home">
+    <header className="App-header">
+      <h1>Hi, I'm Jess</h1>
+    </header>
+  </div>
+);
+}
